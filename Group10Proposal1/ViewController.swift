f7204d802fcbd2d45519c341155460f4802a7cbb
m1n3sweeper12 <greyson.morgan363@gmail.com>
2026-02-12 12:04:13 -0500

    Initial Commit

---
+++ Group10Proposal1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Group10Proposal1
+//
+//  Created by Morgan, Ashley F. on 2/12/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
